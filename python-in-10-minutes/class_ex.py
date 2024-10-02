4b0df1d5182107f029b504bfeef3a02d86c99c97
leonard-seydoux <leonard.seydoux@gmail.com>
2024-10-02 10:21:41 +0200

    Added notebook 4

---
+++ python-in-10-minutes/class_ex.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self):
+        self.age = None
+        self.height = 160
+
+    def __add__(self, other):
+        
+
+    def birthday(self):
+        self.height += 1
